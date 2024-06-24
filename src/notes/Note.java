70132a2112f8c0dd5310a994158e9f732ac86c0e
Yeabkal Biru <yeabkalbiru@gmail.com>
2024-06-24 10:19:48 -0700

    Setting up Note class

--- src/notes/Note.java
+++ src/notes/Note.java
@@ -1,14 +1,38 @@
 package notes;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
 public class Note {
 
     public String noteContent;
 
     public Long noteId;
 
+    public Date dayCreated;
 
-    public Note(final String note, final Long NoteId){
+    public Date noteType;
+
+    public Date lastEdited;
+
+
+
+    public Note(final String note, final Long noteId, final Date dayCreated, final Date noteType, final Date lastEdited){
         this.noteContent = note;
-        this.noteId = NoteId;
+
+        this.noteId = noteId;
+
+        this.dayCreated = dayCreated;
+
+        this.noteType = noteType;
+
+        this.lastEdited = lastEdited;
+
+
     }
+
 }
